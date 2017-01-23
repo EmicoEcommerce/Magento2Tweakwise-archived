93c6db63a8e88712b5cd493c045c08f04d1a0bd4
Freek Gruntjes <fgruntjes@emico.nl>
2017-01-23 12:15:37 +0100

    Initial import

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.1.0-beta (2017-02-01)
+
+Features:
+- Supported Magento versions 100.1.*|100.0.*,
---
+++ src/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tweakwise & Emico (https://www.tweakwise.com/ & https://www.emico.nl/) - All Rights Reserved
+ *
+ * @copyright Copyright (c) 2017-2017 Tweakwise.com B.V. (https://www.tweakwise.com)
+ * @license   Proprietary and confidential, Unauthorized copying of this file, via any medium is strictly prohibited
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Emico_Tweakwise',
+    __DIR__
+);
