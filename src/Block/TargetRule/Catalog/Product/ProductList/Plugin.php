452f14a6cbb860c9e87580311edea84dd70aa726
Freek Gruntjes <fgruntjes@emico.nl>
2017-10-20 23:40:14 +0200

    Moved recommendation request arguments to context class for later use in featured products

--- src/Block/TargetRule/Catalog/Product/ProductList/Plugin.php
+++ src/Block/TargetRule/Catalog/Product/ProductList/Plugin.php
@@ -9,16 +9,12 @@
 namespace Emico\Tweakwise\Block\TargetRule\Catalog\Product\ProductList;
 
 use Closure;
+use Emico\Tweakwise\Exception\InvalidArgumentException;
 use Emico\Tweakwise\Model\Catalog\Product\Recommendation\Collection;
-use Emico\Tweakwise\Model\Catalog\Product\Recommendation\CollectionFactory;
-use Emico\Tweakwise\Model\Client;
+use Emico\Tweakwise\Model\Catalog\Product\Recommendation\Context;
 use Emico\Tweakwise\Model\Client\Request\Recommendations\ProductRequest;
-use Emico\Tweakwise\Model\Client\RequestFactory;
-use Emico\Tweakwise\Model\Client\Response\RecommendationsResponse;
 use Emico\Tweakwise\Model\Config;
-use Magento\Catalog\Model\Config as CatalogConfig;
 use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\Product\Visibility;
 use Magento\Framework\Registry;
 use Magento\TargetRule\Block\Catalog\Product\ProductList\AbstractProductList;
 
@@ -27,42 +23,7 @@ class Plugin
     /**
      * @var string
      */
-    protected $type;
-
-    /**
-     * @var Config
-     */
-    private $config;
-
-    /**
-     * @var Client
-     */
-    private $client;
-
-    /**
-     * @var Registry
-     */
-    private $registry;
-
-    /**
-     * @var RequestFactory
-     */
-    private $requestFactory;
-
-    /**
-     * @var CollectionFactory
-     */
-    private $collectionFactory;
-
-    /**
-     * @var RecommendationsResponse
-     */
-    private $response;
-
-    /**
-     * @var ProductRequest
-     */
-    private $request;
+    private $type;
 
     /**
      * @var Collection
@@ -70,38 +31,34 @@ class Plugin
     private $collection;
 
     /**
-     * @var CatalogConfig
+     * @var Config
      */
-    private $catalogConfig;
+    private $config;
 
     /**
-     * @var Visibility
+     * @var Registry
      */
-    private $visibility;
+    private $registry;
+
+    /**
+     * @var Context
+     */
+    private $context;
 
     /**
      * Plugin constructor.
      *
      * @param string $type
      * @param Config $config
-     * @param Client $client
-     * @param RequestFactory $requestFactory
      * @param Registry $registry
-     * @param CollectionFactory $collectionFactory
-     * @param CatalogConfig $catalogConfig
-     * @param Visibility $visibility
+     * @param Context $context
      */
-    public function __construct($type, Config $config, Client $client, RequestFactory $requestFactory, Registry $registry,
-        CollectionFactory $collectionFactory, CatalogConfig $catalogConfig, Visibility $visibility)
+    public function __construct($type, Config $config, Registry $registry, Context $context)
     {
         $this->type = (string) $type;
         $this->config = $config;
-        $this->client = $client;
         $this->registry = $registry;
-        $this->requestFactory = $requestFactory;
-        $this->collectionFactory = $collectionFactory;
-        $this->catalogConfig = $catalogConfig;
-        $this->visibility = $visibility;
+        $this->context = $context;
     }
 
     /**
@@ -146,54 +103,20 @@ class Plugin
     }
 
     /**
-     * @return ProductRequest
-     */
-    private function getRequest()
-    {
-        if (!$this->request) {
-            $this->request = $this->requestFactory->create();
-            $this->configureRequest($this->request);
-        }
-
-        return $this->request;
-    }
-
-    /**
-     * @return RecommendationsResponse
-     */
-    private function getResponse()
-    {
-        if (!$this->response) {
-            $this->response = $this->client->request($this->getRequest());
-        }
-        return $this->response;
-    }
-
-    /**
      * @return Collection
      */
     private function getCollection()
     {
         if (!$this->collection) {
-            $collection = $this->collectionFactory->create(['response' => $this->getResponse()]);
-            $this->prepareCollection($collection);
-            $this->collection = $collection;
+            $request = $this->context->getRequest();
+            if (!$request instanceof ProductRequest) {
+                throw new InvalidArgumentException('Set context should contain ProductRequest');
+            }
+
+            $this->configureRequest($request);
+            $this->collection = $this->context->getCollection();
         }
 
         return $this->collection;
-    }
-
-    /**
-     * @param Collection $collection
-     */
-    private function prepareCollection(Collection $collection)
-    {
-        $collection->addMinimalPrice()
-            ->addFinalPrice()
-            ->addTaxPercents()
-            ->addAttributeToSelect($this->catalogConfig->getProductAttributes())
-            ->addUrlRewrite()
-            ->setVisibility($this->visibility->getVisibleInCatalogIds())
-            ->setFlag('do_not_use_category_id', true);
     }
 }
---
+++ src/Model/Catalog/Product/Recommendation/Context.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * @author Emico <info@emico.nl>
+ * @copyright (c) Emico B.V. 2017
+ */
+
+namespace Emico\Tweakwise\Model\Catalog\Product\Recommendation;
+
+use Emico\Tweakwise\Model\Client;
+use Emico\Tweakwise\Model\Client\Request\Recommendations\FeaturedRequest;
+use Emico\Tweakwise\Model\Client\RequestFactory;
+use Emico\Tweakwise\Model\Client\Response\RecommendationsResponse;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\Config as CatalogConfig;
+
+class Context
+{
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var RequestFactory
+     */
+    private $requestFactory;
+
+    /**
+     * @var CollectionFactory
+     */
+    private $collectionFactory;
+
+    /**
+     * @var CatalogConfig
+     */
+    private $catalogConfig;
+
+    /**
+     * @var Visibility
+     */
+    private $visibility;
+
+    /**
+     * @var FeaturedRequest
+     */
+    private $request;
+
+    /**
+     * @var RecommendationsResponse
+     */
+    private $response;
+
+    /**
+     * @var Collection
+     */
+    private $collection;
+
+    /**
+     * Context constructor.
+     * @param Client $client
+     * @param RequestFactory $requestFactory
+     * @param CollectionFactory $collectionFactory
+     * @param CatalogConfig $catalogConfig
+     * @param Visibility $visibility
+     */
+    public function __construct(Client $client, RequestFactory $requestFactory, CollectionFactory $collectionFactory,
+        CatalogConfig $catalogConfig, Visibility $visibility)
+    {
+        $this->client = $client;
+        $this->requestFactory = $requestFactory;
+        $this->collectionFactory = $collectionFactory;
+        $this->catalogConfig = $catalogConfig;
+        $this->visibility = $visibility;
+    }
+
+    /**
+     * @return FeaturedRequest
+     */
+    public function getRequest()
+    {
+        if (!$this->request) {
+            $this->request = $this->requestFactory->create();
+        }
+
+        return $this->request;
+    }
+
+    /**
+     * @return RecommendationsResponse
+     */
+    public function getResponse()
+    {
+        if (!$this->response) {
+            $this->response = $this->client->request($this->getRequest());
+        }
+        return $this->response;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getCollection()
+    {
+        if (!$this->collection) {
+            $collection = $this->collectionFactory->create(['response' => $this->getResponse()]);
+            $this->prepareCollection($collection);
+            $this->collection = $collection;
+        }
+
+        return $this->collection;
+    }
+
+    /**
+     * @param Collection $collection
+     */
+    private function prepareCollection(Collection $collection)
+    {
+        $collection->addMinimalPrice()
+            ->addFinalPrice()
+            ->addTaxPercents()
+            ->addAttributeToSelect($this->catalogConfig->getProductAttributes())
+            ->addUrlRewrite()
+            ->setVisibility($this->visibility->getVisibleInCatalogIds())
+            ->setFlag('do_not_use_category_id', true);
+    }
+}
