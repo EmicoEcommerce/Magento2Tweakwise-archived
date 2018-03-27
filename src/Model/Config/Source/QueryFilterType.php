bf3e2705222f09aa874d64e1341ac155d2a3e10e
Freek Gruntjes <fgruntjes@emico.nl>
2018-03-27 15:53:06 +0200

    Added query argument filter options

---
+++ src/Model/Config/Source/QueryFilterType.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Tweakwise & Emico (https://www.tweakwise.com/ & https://www.emico.nl/) - All Rights Reserved
+ *
+ * @copyright Copyright (c) 2017-2017 Tweakwise.com B.V. (https://www.tweakwise.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Emico\Tweakwise\Model\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class QueryFilterType implements ArrayInterface
+{
+    /**
+     * Possible filter types
+     */
+    const TYPE_SPECIFIC = 'specific';
+    const TYPE_REGEX = 'regex';
+    const TYPE_NONE = 'none';
+
+    /**
+     * @var array[]
+     */
+    protected $options;
+
+    /**
+     * @return array
+     */
+    protected function buildOptions()
+    {
+        return [
+            ['value' => self::TYPE_NONE, 'label' => __('Dont\'t filter')],
+            ['value' => self::TYPE_SPECIFIC, 'label' => __('Specific arguments')],
+            ['value' => self::TYPE_REGEX, 'label' => __('Regex matching')],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        if (!$this->options) {
+            $this->options = $this->buildOptions();
+        }
+        return $this->options;
+    }
+}
