3f8a8e1376d850416fc2f7b839efab8dcf6752de
Edwin Jacobs <ejacobs@emico.nl>
2019-10-23 22:36:11 +0200

    Fixed issue where Interceptor was NOT created with di:compile in production mode, (it worked in developer mode ....) The issue was that the plugin we defined had a virtualType as type argument, di compile does not reslove the concrete class when determining if some method should have an interceptor. This would lead to no related or upsell recommendations as the plugin would not be triggered

---
+++ src/Block/TargetRule/Catalog/Product/ProductList/RelatedPlugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author : Edwin Jacobs, email: ejacobs@emico.nl.
+ * @copyright : Copyright Emico B.V. 2019.
+ */
+declare(strict_types=1);
+
+
+namespace Emico\Tweakwise\Block\TargetRule\Catalog\Product\ProductList;
+
+
+class RelatedPlugin extends Plugin
+{
+    protected $type = 'crosssell';
+}
