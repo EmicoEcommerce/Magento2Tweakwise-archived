ab846b1f9284dc73408f93a0171247abe038cdb4
Herant <herant.awadisan@gmail.com>
2021-11-24 14:40:28 +0100

    Render no result response if filter yields no result

--- view/frontend/web/js/navigation-form.js
+++ view/frontend/web/js/navigation-form.js
@@ -19,6 +19,10 @@ define([
             filterSelector: '#layered-filter-block',
             productListSelector: '.products.wrapper',
             toolbarSelector: '.toolbar.toolbar-products',
+            productsGridSelector: '.products-grid',
+            mainColumnSelector: '.column.main',
+            noProductsSelector: '.message.info.empty',
+            noResultsSelector: '.message.notice',
             isLoading: false,
         },
 
@@ -252,6 +256,11 @@ define([
             }
 
             var toolbar = $(toolbarSelector);
+            const productsGrid = $(this.options.productsGridSelector);
+            const mainColumn = $(this.options.mainColumnSelector);
+            const noProducts = $(this.options.noProductsSelector);
+            const noResult = $(this.options.noResultsSelector);
+
             /*
             The product list comes after the toolbar.
             We use this construction as there could be more product lists on the page
@@ -261,6 +270,19 @@ define([
                 toolbar
                     .siblings(productListSelector)
                     .replaceWith(newProductList);
+                noProducts.remove();
+                noResult.remove();
+            } else {
+                /*
+                It happens that a filter yields no result.
+                In that case magento returns a message in a div
+                that needs to rendered correctly and also removed if not required
+                */
+                toolbar.hide();
+                productsGrid.hide();
+                $('script[data-role=\'msrp-popup-template\']').remove();
+                $('script[data-role=\'msrp-info-template\']').remove();
+                mainColumn.append(htmlResponse);
             }
             if (newToolbarFirst) {
                 toolbar
@@ -280,7 +302,7 @@ define([
                     .replaceWith(newToolbarLast.outerHTML + scripts);
             }
 
-	    const primaryActionsDiv = $(".actions-primary")
+            const primaryActionsDiv = $(".actions-primary")
             primaryActionsDiv.find('form').
             each(function (i, form) {
                 $(form).append('<input name="form_key" type="hidden" ' +
@@ -336,3 +358,5 @@ define([
 
     return $.tweakwise.navigationForm;
 });
+
+
